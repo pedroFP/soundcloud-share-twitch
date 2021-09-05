99c04e2a215ea5547edafab73772b608cdaed9b5
Pedro <pedro@vinkodigital.com>
2021-09-05 13:13:56 -0300

    💄 add font awesome 5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import "@fortawesome/fontawesome-free/css/all"
 import "bulma"
 require("@rails/ujs").start()
 require("turbolinks").start()
