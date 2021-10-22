03114620b6d1c8f8454f7844570f833f06811c94
Pedro <pedro@vinkodigital.com>
2021-10-22 00:09:36 -0300

    🔧 (routes): add tracks routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'tracks/index'
-  resources :streams
   root to: 'home#index'
   devise_for :viewers
   devise_for :admins
+  resources :streams
+  resources :tracks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
