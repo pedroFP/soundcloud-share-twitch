329064c6b7b75a1a80628e588573290801f9bf5e
Pedro <pedro@vinkodigital.com>
2021-10-25 01:47:20 -0300

    ✨ (twitch login): add helper to build the login with twitch url

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,12 @@
 module TracksHelper
+  def twitch_login_url
+    twitch_url = 'https://id.twitch.tv/oauth2/authorize'
+    client_id = "client_id=#{ENV['TWITCH_CLIENT_ID']}"
+    redirect_url = "redirect_uri=#{ENV['TWITCH_REDIRECT_URL']}"
+    response_type = 'response_type=token'
+    scope = 'scope=channel_read%20user_subscriptions'
+    state = 'state=logged_in'
+
+    "#{twitch_url}?#{client_id}&#{redirect_url}&#{response_type}&#{scope}&#{state}"
+  end
 end
