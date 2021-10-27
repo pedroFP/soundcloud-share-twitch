4609ddb09fa32615f53d9a59a325f192a9e57dde
Pedro <pedro@vinkodigital.com>
2021-10-27 00:30:30 -0300

    ♻️ (hosts) accept ngrok hosts on development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  config.hosts << '55de-2800-150-13d-aa3-a1dc-a0d6-942b-f9ea.ngrok.io'
+  config.hosts << /.*\.ngrok\.io/
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
