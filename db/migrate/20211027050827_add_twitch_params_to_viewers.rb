d479bf791f3bcb9ad15bf45309e4539b9628f8b0
Pedro <pedro@vinkodigital.com>
2021-11-05 20:20:31 -0300

    🔥 (viewers): move merge migrations

--- db/migrate/20211027050827_add_twitch_params_to_viewers.rb
+++ db/migrate/20211027050827_add_twitch_params_to_viewers.rb
@@ -3,5 +3,6 @@ class AddTwitchParamsToViewers < ActiveRecord::Migration[6.0]
     add_column :viewers, :twitch_id, :string
     add_column :viewers, :twitch_login, :string
     add_column :viewers, :twitch_display_name, :string
+    add_column :viewers, :profile_image_url, :string
   end
 end
--- db/migrate/20211105215259_add_profile_image_url_to_viewer.rb
+++
@@ -1,5 +0,0 @@
-class AddProfileImageUrlToViewer < ActiveRecord::Migration[6.0]
-  def change
-    add_column :viewers, :profile_image_url, :string
-  end
-end
