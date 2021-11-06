db3c94d992d16ad8903f42e21419677fb392b5ba
Pedro <pedro@vinkodigital.com>
2021-11-06 19:28:53 -0300

    ♻️ (tracks): refactored tracks subs sorter

--- app/javascript/packs/tracks.js
+++ app/javascript/packs/tracks.js
@@ -1,3 +1,4 @@
+// Sort tracks by subscribed viewers
 $(document).on('change', '.sort-by-sub:checkbox', (event) => {
   const element = $(event.target)
   $.ajax({
@@ -8,4 +9,11 @@ $(document).on('change', '.sort-by-sub:checkbox', (event) => {
       sortBySub: element.prop('checked')
     },
   });
+})
+
+// Display tracks sorter checked or not
+$(document).on('turbolinks:load', function () {
+  params = new URLSearchParams(window.location.search)
+
+  $('.sort-by-sub:checkbox').prop('checked', params.get('sortBySub') == 'true')
 })
