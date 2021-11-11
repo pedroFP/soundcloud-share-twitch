bb92726be985736bcca32690b0e64901efc5498d
Pedro <pedro@vinkodigital.com>
2021-11-10 22:29:31 -0300

    ✅ (tracks): update controller and models tests

--- test/controllers/tracks_controller_test.rb
+++ test/controllers/tracks_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class TracksControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get tracks_index_url
+    get tracks_url
     assert_response :success
   end
 
--- test/models/track_test.rb
+++ test/models/track_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class TrackTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'it should not be valid' do
+    track = Track.new
+    assert_not track.valid?
+  end
+
+  test 'it should have a viewer' do
+    track = Track.new
+
+    assert_not track.valid?
+  end
+
+  test 'it should have the sound cloud url' do
+    track = Track.new
+    track.viewer = viewers(:not_sub)
+
+    assert_not track.valid?
+  end
+
+  test 'it should be valid' do
+    track = Track.new
+    track.soundcloud_url = 'some_url'
+    track.viewer = viewers(:not_sub)
+
+    assert track.valid?
+  end
 end
