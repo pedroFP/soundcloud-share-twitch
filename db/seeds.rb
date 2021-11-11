6e182215eb1a0f4555808c309b4f434b5bfdcfc3
Pedro <pedro@vinkodigital.com>
2021-11-10 22:34:19 -0300

    🌱 add commented seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# SOUND_CLOUD_URL = '<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1153842502&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/sheepnmusic" title="Sheepn" target="_blank" style="color: #cccccc; text-decoration: none;">Sheepn</a> · <a href="https://soundcloud.com/sheepnmusic/no-longer-together" title="No Longer Together (with TATiANNA)" target="_blank" style="color: #cccccc; text-decoration: none;">No Longer Together (with TATiANNA)</a></div>'.freeze
+# viewer = Viewer.first
+# 20.times do
+#   Track.create(
+#     viewer: viewer,
+#     soundcloud_url: SOUND_CLOUD_URL
+#   )
+# end
