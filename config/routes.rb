320fb9d2f8af66edd8ecfec20b0e5f5128ce67b4
Pedro <pedro@vinkodigital.com>
2021-11-13 22:56:34 -0300

    🚧  (samples): create controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :tracks, only: %i[index show]
   resources :streams do
     resources :tracks
+    resources :samples
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
