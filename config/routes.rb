29784f8119cb91a2cdabb027ccd6070ecd2487af
Pedro <pedro@vinkodigital.com>
2021-12-02 01:13:28 -0300

    🔥 (tarcks): deleted show endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   post 'twitch/sign_user'
   devise_for :viewers, skip: :all
   devise_for :admins
-  resources :tracks, only: %i[index show]
+  resources :tracks, only: %i[index]
   resources :streams do
-    resources :tracks, only: %i[index show new create]
+    resources :tracks, only: %i[index new create]
     resources :samples, only: %i[create destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
