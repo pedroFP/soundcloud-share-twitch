d81b8655585275b56168b9cc8cb99aef8290fdde
Pedro <pedro@vinkodigital.com>
2021-12-02 01:14:01 -0300

    🔒️ (tracks): update policy

--- app/policies/track_policy.rb
+++ app/policies/track_policy.rb
@@ -4,4 +4,16 @@ class TrackPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def index?
+    true
+  end
+
+  def new?
+    user.is_a?(Viewer)
+  end
+
+  def create?
+    user.is_a?(Viewer)
+  end
 end
