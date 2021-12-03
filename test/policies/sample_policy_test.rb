3f915580933d1c5e443f849fdf2f7235be7c96ec
Pedro <pedro@vinkodigital.com>
2021-12-03 00:38:52 -0300

    ✅ (samples): test policies

--- test/policies/sample_policy_test.rb
+++ test/policies/sample_policy_test.rb
@@ -1,18 +1,31 @@
 require 'test_helper'
 
 class SamplePolicyTest < ActiveSupport::TestCase
-  def test_scope
+
+  test 'Viewers should not create samples' do
+    @viewer = viewers(:not_sub)
+    @sample = Sample.new(name: 'Sample', link: 'https"//www.test.com')
+
+    refute_permit @viewer, @sample, :create
   end
 
-  def test_show
+  test 'Admins should create samples' do
+    @admin = admins(:one)
+    @sample = Sample.new(name: 'Sample', link: 'https"//www.test.com')
+
+    assert_permit @admin, @sample, :create
+  end
+  test 'Viewers should not destroy samples' do
+    @viewer = viewers(:not_sub)
+    @sample = samples(:one)
+
+    refute_permit @viewer, @sample, :destroy
   end
 
-  def test_create
-  end
+  test 'Admins should destroy samples' do
+    @admin = admins(:one)
+    @sample = samples(:one)
 
-  def test_update
-  end
-
-  def test_destroy
+    assert_permit @admin, @sample, :destroy
   end
 end
