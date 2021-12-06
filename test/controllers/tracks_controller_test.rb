9d1ecbf8c6f3ac49d55746aac27f3537a055e4c6
Pedro <pedro@vinkodigital.com>
2021-12-06 01:47:49 -0300

    ✅ (tracks): add controller tests

--- test/controllers/tracks_controller_test.rb
+++ test/controllers/tracks_controller_test.rb
@@ -1,9 +1,69 @@
 require 'test_helper'
 
 class TracksControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  include Devise::Test::IntegrationHelpers
+
+  test "Anyone should get index" do
     get tracks_url
     assert_response :success
   end
 
+  test "Viewer should get new" do
+    @viewer = viewers(:not_sub)
+    @stream = streams(:one)
+    sign_in @viewer
+
+    get new_stream_track_path(@stream)
+
+    assert_response :success
+  end
+
+  test "Viewer should create tracks" do
+    @viewer = viewers(:not_sub)
+    @stream = streams(:one)
+    sign_in @viewer
+
+    track_params = {
+      soundcloud_url: 'lorem',
+      stream_id: @stream.id,
+      viewer_id: @viewer.id
+    }
+
+    post stream_tracks_path(@stream), params: {
+      track: track_params
+    }
+
+    assert_response :redirect
+  end
+
+  test 'Admin should not get new' do
+    @admin = admins(:one)
+    @stream = streams(:one)
+    sign_in @admin
+
+    get new_stream_track_path(@stream)
+
+    assert_response :redirect
+  end
+
+  test 'Admin should create tracks' do
+    @admin = admins(:one)
+    @stream = streams(:one)
+    tracks_count = Track.count
+    sign_in @admin
+
+    track_params = {
+      soundcloud_url: 'lorem',
+      stream_id: @stream.id,
+      viewer_id: viewers(:not_sub).id
+    }
+
+    post stream_tracks_path(@stream), params: {
+      track: track_params
+    }
+
+    assert_equal tracks_count, Track.count
+
+    assert_response :redirect
+  end
 end
