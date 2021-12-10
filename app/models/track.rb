2ae769c7cb35e031774b76d635f8b92dfd5c3afd
Pedro <pedro@vinkodigital.com>
2021-12-10 03:00:31 -0300

    🔒️ (track): add soundcloud url regex

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,8 @@
 class Track < ApplicationRecord
+  SOUND_CLOUD_REGEX = /\Ahttps?:\/\/[soundcloud.com]+\/[\w]+\/[\w]/
+
   belongs_to :viewer
   belongs_to :stream
 
-  validates :soundcloud_url, presence: true
+  validates :soundcloud_url, presence: true, format: { with: SOUND_CLOUD_REGEX }
 end
