1f0177b6295b2348e20b2ced3eb60b9e68efc422
Pedro <pedro@vinkodigital.com>
2022-01-24 00:47:48 -0300

    🐛 (streams): fixed show controller liked tracks

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -20,7 +20,7 @@ class StreamsController < ApplicationController
     @new_stream_track = @stream.tracks.build
     @tracks = @stream.tracks.order('created_at desc').includes(:viewer)
     @tracks = @tracks.reorder('likes_count desc') if params['sortByLikes'] == 'true'
-    @current_viewer_liked_tracks = current_viewer.liked_tracks.ids
+    @current_viewer_liked_tracks = viewer_signed_in? ? current_viewer.liked_tracks.ids : []
     @pagy, @tracks = pagy(@tracks)
   end
 
